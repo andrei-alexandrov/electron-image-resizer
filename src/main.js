b644bd06170d4f394a701d50fe721c760da390e2
Andrei Alexandrov <andrei.alxv@gmail.com>
2024-02-21 20:33:58 +0200

    added image upload functionality + showing the dimensions of each image and name

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, Menu } = require("electron");
 const path = require("path");
 
 const isMac = process.platform === "darwin";
@@ -7,11 +7,13 @@ if (require("electron-squirrel-startup")) {
   app.quit();
 }
 
+//Creating the main window
 const createMainWindow = () => {
   const mainWindow = new BrowserWindow({
     title: "Image Resizer",
     width: 1000,
     height: 600,
+    icon: path.join(__dirname, "../assets/icons/favicon.ico"),
     webPreferences: {
       preload: path.join(__dirname, "preload.js"),
     },
@@ -21,9 +23,24 @@ const createMainWindow = () => {
   mainWindow.webContents.openDevTools();
 };
 
-// app.on("ready", createWindow);
+//Creating the about window
+const createAboutWindow = () => {
+  const aboutWindow = new BrowserWindow({
+    title: "",
+    width: 400,
+    height: 300,
+  });
+
+  aboutWindow.loadFile(path.join(__dirname, "./renderer/about.html"));
+};
+
+//App is ready
 app.whenReady().then(() => {
   createMainWindow();
+
+  //Implement menu
+  const mainMenu = Menu.buildFromTemplate(menuTemplate);
+  Menu.setApplicationMenu(mainMenu);
 
   app.on("activate", () => {
     if (BrowserWindow.getAllWindows().length === 0) {
@@ -31,6 +48,46 @@ app.whenReady().then(() => {
     }
   });
 });
+
+const menuTemplate = [
+  {
+    label: "File",
+    submenu: [
+      {
+        label: "Quit",
+        click: () => {
+          app.quit();
+        },
+        accelerator: "Ctrl + q",
+      },
+    ],
+  },
+  ...(isMac
+    ? [
+        {
+          label: app.name,
+          submenu: [
+            {
+              label: "About",
+              click: createAboutWindow,
+            },
+          ],
+        },
+      ]
+    : [
+        {
+          label: "Help",
+          submenu: [
+            {
+              label: "About",
+              click: () => {
+                createAboutWindow();
+              },
+            },
+          ],
+        },
+      ]),
+];
 
 app.on("window-all-closed", () => {
   if (!isMac) {
---
+++ src/renderer/js/renderer.js
@@ -0,0 +1,36 @@
+const form = document.getElementById("img-form");
+const img = document.getElementById("img");
+const outputPath = document.getElementById("output-path");
+const filename = document.getElementById("filename");
+const heightInput = document.getElementById("height");
+const widthInput = document.getElementById("width");
+
+const loadImage = (e) => {
+  const file = e.target.files[0]; //Can i do also img.files[0]; ?
+
+  if (!isFileImage(file)) {
+    alert("Please select an image. Acceptable formats are: gif, png, jpeg");
+    return;
+  }
+  console.log("Success");
+
+  //Here i get the orignal dimensions
+  const image = new Image();
+  image.src = URL.createObjectURL(file);
+  image.onload = function () {
+    widthInput.value = this.width;
+    heightInput.value = this.height;
+  };
+
+  form.style.display = "block";
+  filename.innerText = file.name;
+};
+
+//I am making sure that the file is an image
+const isFileImage = (file) => {
+  const acceptedImageTypes = ["image/gif", "image/jpeg", "image/png", "image/jpg", "image/webp"];
+  return file && acceptedImageTypes.includes(file["type"]);
+};
+
+img.addEventListener("change", loadImage);
+
